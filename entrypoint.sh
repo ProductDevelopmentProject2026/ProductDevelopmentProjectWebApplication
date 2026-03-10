0a5e1e32a8319e29dec467c191770262de591115
Arsenii Ziubin <arsenziubin@gmail.com>
2026-03-10 14:25:06 +0200

    changed command to start web application on cloud to have few threads and timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,5 @@ echo "Collecting static files"
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn"
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8080
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8080 --workers 3 --threads 4 --timeout 120
+#exec gunicorn core.wsgi:application --bind 0.0.0.0:8080
